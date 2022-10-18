66d622362d45df5b668326f0b0d81e6b52d684a1
Joe Capra <jcapra@160over90.com>
2022-10-18 09:07:43 -0700

    update styles

--- src/components/AdDisplay/index.js
+++ src/components/AdDisplay/index.js
@@ -1,18 +1,14 @@
-import './styles.scss'
-import React, { useEffect, useState } from 'react'
-
 import Ad from '../Ad'
 import { v4 as uuidv4 } from 'uuid'
 
+import './styles.scss'
+
 function AdDisplay({ targets, urlParams }) {
-	console.error('AD DISPLAY TARGET PROP===', targets)
 	return (
 		<div className="addisplay">
-			<div className="props__adcontainer">
-				{targets.map(target => {
-					return <Ad key={uuidv4()} target={target} urlParams={urlParams} />
-				})}
-			</div>
+			{targets.map(target => {
+				return <Ad key={uuidv4()} target={target} urlParams={urlParams} />
+			})}
 		</div>
 	)
 }
