8a3a44315d3997a8036b4a7f80213480d30aab60
Joe Capra <jcapra@160over90.com>
2022-10-17 23:39:05 -0700

    test

--- src/components/AdDisplay/index.js
+++ src/components/AdDisplay/index.js
@@ -5,13 +5,13 @@ import Ad from '../Ad'
 import { v4 as uuidv4 } from 'uuid'
 
 function AdDisplay({ targets, urlParams, deployFolder }) {
-	console.error('AD DISPLAY TARGET PROP==='.targets)
+	console.error('AD DISPLAY TARGET PROP===', targets)
 	return (
 		<div className="addisplay">
 			<div className="props__adcontainer">
-				{targets.map(target => {
+				{/* {targets.map(target => {
 					return <Ad key={uuidv4()} data={target} urlParams={urlParams} deployFolder={deployFolder} />
-				})}
+				})} */}
 			</div>
 		</div>
 	)
